3d5f5af196026998f7d393c4e7c36d62ad3022f8
TrebroNm <81372891+TrebroNm@users.noreply.github.com>
2022-03-24 12:46:57 +0100

    InvoiceDaoImplementation

--- ProjectPersistance/src/DAO/DaoInvoice.java
+++ ProjectPersistance/src/DAO/DaoInvoice.java
@@ -1,5 +1,23 @@
 package DAO;
 
-public class DaoInvoice {
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import model.order.Invoice;
+
+public interface DaoInvoice{
+	
+	public void create(Invoice invoice);
+	
+	public Invoice read(String id) throws SQLException;
+	
+	public void update(Invoice invoice);
+	
+	public void delete(Invoice invoice);
+	
+	public Invoice findById(String id);
+	
+	public ArrayList<Invoice> readAll();
+	
+	
 }
