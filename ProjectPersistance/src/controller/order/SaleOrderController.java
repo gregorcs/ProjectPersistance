77885d85f32112a424103825040c671fcfde151b
TrebroNm <81372891+TrebroNm@users.noreply.github.com>
2022-03-24 12:07:20 +0100

    DAOPackage

--- ProjectPersistance/src/controller/order/SaleOrderController.java
+++ ProjectPersistance/src/controller/order/SaleOrderController.java
@@ -1,16 +1,18 @@
 package controller.order;
 
+import model.order.Invoice;
 import model.order.SalesOrder;
 
 public class SaleOrderController {
 
 	private SalesOrder order;
+	private Invoice invoice;
 	
 	public void createSalesOrder() {
-		
+		order = new SalesOrder();
 	}
 	
 	public void createInvoice() {
-		
+		invoice = new Invoice();
 	}
 }
